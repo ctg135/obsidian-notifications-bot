f6776e0a409d8914ceab30c79dc1ecd68b56e24e
ctg135 <salabaevr@yandex.ru>
2024-09-16 16:42:29 +0300

    added webdav communication

--- src/daily.py
+++ src/daily.py
@@ -1,9 +1,48 @@
 
 import utils
+from webdav3.client import Client
 
 # Loading configuration file
 conf = utils.load_pass_file("pass.json")
 
-# TODO: open connection to Webdav server
+utils.check_tmp_folder_exists(conf['tmp_dir'])
+tmp_file_name = conf['tmp_dir'] + 'temp_file'
+# Connection options for Webdav server
+options = {
+    'webdav_hostname': conf['server'],
+    'webdav_login': conf['login'],
+    'webdav_password': conf['password']
+}
 
+root_dir = conf['vault_name'] + '/'
+vault = Client(options)
 
+if not vault.check(root_dir):
+    print(f'Folder "{root_dir}" does not exist at WebDav vault')
+
+def look_dirs(base_dir: str):
+    items = vault.list(base_dir)
+    # print(f'Look up in "{base_dir}": {items}')
+    for item in items:
+        # item is note
+        # need to download and look up for tags
+        # print(item[-3::])
+        if item[-3::] == '.md':
+            print(f'Found .md file: {item}')
+            check_notifications(vault, base_dir + item)
+        # item is directory
+        if item[-1] == '/':
+            print(f'Found directory: {item}')
+            look_dirs(base_dir + item)
+
+def check_notifications(server, filename: str):
+    server.download_sync(remote_path=filename, local_path=tmp_file_name)
+    exit()
+
+look_dirs(root_dir)
+
+#TODO: download file and search tags in there
+
+#TODO: send messages
+
+#TODO: проверка существования временного каталога
--- src/utils.py
+++ src/utils.py
@@ -14,7 +14,15 @@ def load_pass_file(filename: str):
     with open(filename, 'r') as file:
         conf = json.load(file)
     
-    keys = ['vault_name', 'bot_id', 'user_id', 'server', 'login', 'password']
+    keys = [
+        'vault_name', 
+        'bot_id', 
+        'user_id', 
+        'server', 
+        'login', 
+        'password',
+        'tmp_dir'
+        ]
 
     error_flag = False
     for key in keys:
@@ -25,3 +33,12 @@ def load_pass_file(filename: str):
     if error_flag: exit()
 
     return conf
+
+def check_tmp_folder_exists(path: str):
+    '''
+    Checks that folder exists, if not, creates folder
+    '''
+    if not os.path.isdir(path):
+        print('Path {path} not exists, creating folder')
+        os.mkdir(path)
+
